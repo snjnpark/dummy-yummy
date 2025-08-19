be00adcfe4f8df8c09ed3c83e16579ab5d75718a
snjnpark <hello@snjnpark.kr>
2025-08-19 20:05:47 +0900

    Initial commit

---
+++ popup.js
@@ -0,0 +1,8 @@
+document.getElementById('fill-form').addEventListener('click', () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      files: ['content.js']
+    });
+  });
+});
